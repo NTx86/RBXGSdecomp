96b800c78ebbd784ca9c41f44ff50c29cadeda4a
NT_x86 <sumoner200@gmail.com>
2024-10-31 15:43:25 +0200

    Fill out Velocity.h function definitions

--- Client/App/include/util/Velocity.h
+++ Client/App/include/util/Velocity.h
@@ -7,7 +7,16 @@ namespace RBX {
 		public:
 			G3D::Vector3 linear;
 			G3D::Vector3 rotational;
-			Velocity::Velocity():linear(Vector3::zero()),rotational(Vector3::zero())
-			{}
+			bool operator==(const Velocity&) const;
+			bool operator!=(const Velocity&) const;
+			Velocity(const G3D::Vector3&, const G3D::Vector3&);
+			Velocity():linear(Vector3::zero()),rotational(Vector3::zero()) {}
+			Velocity operator+(const Velocity&) const;
+			Velocity operator-() const;
+			G3D::Vector3 linearVelocityAtOffset(const G3D::Vector3&) const;
+			Velocity velocityAtOffset(const G3D::Vector3&) const;
+			Velocity rotateBy(const G3D::Matrix3&) const;
+
+			static const Velocity& zero();
 	};
 }
