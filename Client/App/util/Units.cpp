f65ae5f6e9b51cbd791153922612337619657bdc
NT_x86 <sumoner200@gmail.com>
2024-11-01 18:09:17 +0200

    Specify class for kmsAccelerationToRbx

--- Client/App/util/Units.cpp
+++ Client/App/util/Units.cpp
@@ -2,7 +2,7 @@
 
 using namespace RBX;
 
-G3D::Vector3 kmsAccelerationToRbx(const G3D::Vector3& kmsAccel)
+G3D::Vector3 Units::kmsAccelerationToRbx(const G3D::Vector3& kmsAccel)
 {
 	return Vector3(kmsAccel.x * 20.0f, kmsAccel.y * 20.0f, kmsAccel.z * 20.0f);
 }
