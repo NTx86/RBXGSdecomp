0dd8c444c9688e86889065fa5323800e12802b63
KeyboardCombination <gamerbear256@gmail.com>
2025-08-06 05:23:23 -0400

    Contact
    Contact constructor, Contact::putInKernel 100% match, Contact::removeFromKernel 100% match

---
+++ Client/App/Color.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <g3d/color3.h>
+
+namespace RBX
+{
+	class Color { /* Size=0xc */
+	public:
+		static const G3D::Color3 lightGreen()
+		{ 
+			return G3D::Color3(0.50588238, 0.77254903, 0.086274512);
+		}
+	};
+}
--- Client/App/v8world/Contact.cpp
+++ Client/App/v8world/Contact.cpp
@@ -1,1 +1,32 @@
 #include "v8world/Contact.h"
+
+namespace RBX
+{
+	Contact::Contact(Primitive* prim0, Primitive* prim1)
+		: Edge(prim0, prim1),
+		jointK(0),
+		elasticJointK(0),
+		lastContactStep(-1),
+		steppingIndex(-1),
+		kFriction(0)
+	{
+	}
+
+	void Contact::putInKernel(Kernel* _kernel)
+	{
+		IPipelined::putInKernel(_kernel);
+		onPrimitiveContactParametersChanged();
+	}
+
+	void Contact::removeFromKernel()
+	{
+		RBXASSERT(IPipelined::getKernel());
+
+		deleteAllConnectors();
+		IPipelined::removeFromKernel();
+	}
+
+	Contact::~Contact()
+	{
+	}
+}
