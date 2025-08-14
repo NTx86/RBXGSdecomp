e497d4fd6624222d3a04da08f87488fa621c1ddd
Harley <77483822+BigMark824@users.noreply.github.com>
2025-08-14 21:04:20 +1000

    100% isScript match, polish isRobloxSite & change project to multi-byte character set compilation

--- Client/App/include/util/Http.h
+++ Client/App/include/util/Http.h
@@ -1,15 +1,16 @@
 #pragma once
 #include <string>
 #include <windows.h>
-#include <atlbase.h>
 #include <atlutil.h>
+#include <atlstr.h>
 namespace RBX
 {
 	class Http 
 	{
 	public: 
 		Http(const std::string* url);
-		bool isRobloxSite(const wchar_t* url);
+		static bool isRobloxSite(const char* url);
+		static bool isScript(const char* url);
 	private:
 		std::string url;
 		std::string additionalHeaders;
--- Client/App/util/Http.cpp
+++ Client/App/util/Http.cpp
@@ -7,10 +7,11 @@ namespace RBX
 	{
 	}
 
-	bool isRobloxSite(const wchar_t* url)
+	bool isRobloxSite(const char* url)
 	{
+		// i genuinely hate windows APIs and datatypes
 		ATL::CUrl targetUrl;
-		targetUrl.CrackUrl(url);
+		targetUrl.CrackUrl(url); 
 
 		if ( targetUrl.GetScheme()
 			&& (targetUrl.GetScheme() <= ATL_URL_SCHEME_GOPHER || targetUrl.GetScheme() > ATL_URL_SCHEME_HTTPS) )
@@ -18,15 +19,35 @@ namespace RBX
 			return false;
 		}
 
-		LPCTSTR hostName = targetUrl.GetHostName();
-		CStringA host(hostName);
-		host.MakeLower();
+		CString hostName = targetUrl.GetHostName();
+		hostName.MakeLower();
 
-		if (host.Right(10) == "roblox.com")
+		if (hostName.Right(10) == "roblox.com")
 			return true;
-		else if (host.Right(12) == "72.32.62.210")
+		else if (hostName.Right(12) == "72.32.62.210")
 			return true;
-		else if (host.Right(16) == "robloxopolis.com")
+		else if (hostName.Right(16) == "robloxopolis.com")
+			return true;
+
+		return false;
+	}
+
+	bool isScript(const char* url)
+	{
+		ATL::CUrl targetUrl;
+		targetUrl.CrackUrl(url);
+
+		if(targetUrl.GetScheme() == ATL_URL_SCHEME_FTP 
+			|| targetUrl.GetScheme() > ATL_URL_SCHEME_GOPHER && targetUrl.GetScheme() <= ATL_URL_SCHEME_HTTPS )
+		{
+			return false;
+		}
+
+		CString host(url);
+
+		if(host.Find("javascript:") == 0)
+			return true;
+		if(host.Find("jscript:") == 0)
 			return true;
 
 		return false;
