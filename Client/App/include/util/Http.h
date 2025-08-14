4bf80464aa3d47a19e7dbbb560fc281b3c9d9618
Harley <77483822+BigMark824@users.noreply.github.com>
2025-08-14 19:06:09 +1000

    86% Match Http::isRobloxSite

--- Client/App/include/util/Http.h
+++ Client/App/include/util/Http.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <string>
+#include <windows.h>
+#include <atlbase.h>
+#include <atlutil.h>
+#include <wincrypt.h> 
 namespace RBX
 {
 	class Http 
 	{
 	public: 
 		Http(const std::string* url);
+		bool isRobloxSite(const wchar_t* url);
 	private:
 		std::string url;
 		std::string additionalHeaders;
--- Client/App/util/Http.cpp
+++ Client/App/util/Http.cpp
@@ -3,8 +3,32 @@
 namespace RBX
 {
 	Http::Http(const std::string* url) 
-		:url(*url),
-		additionalHeaders()
+		:url(*url)
 	{
+	}
+
+	bool isRobloxSite(const wchar_t* url)
+	{
+		ATL::CUrl targetUrl;
+		targetUrl.CrackUrl(url);
+
+		if ( targetUrl.GetScheme()
+			&& (targetUrl.GetScheme() <= ATL_URL_SCHEME_GOPHER || targetUrl.GetScheme() > ATL_URL_SCHEME_HTTPS) )
+		{
+			return false;
+		}
+
+		LPCTSTR hostName = targetUrl.GetHostName();
+		CStringA host(hostName);
+		host.MakeLower();
+
+		if (host.Right(10) == "roblox.com")
+			return true;
+		else if (host.Right(12) == "72.32.62.210")
+			return true;
+		else if (host.Right(16) == "robloxopolis.com")
+			return true;
+
+		return false;
 	}
 }
