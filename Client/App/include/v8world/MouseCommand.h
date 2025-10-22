25e17951be11e2b60afd71d328ca13985e2a921d
ThePlayerRolo <thesolis78243@gmail.com>
2025-10-22 17:48:06 -0400

    Update header

--- Client/App/include/v8world/MouseCommand.h
+++ Client/App/include/v8world/MouseCommand.h
@@ -6,16 +6,31 @@
 namespace RBX {
 	class XmlState;
 	class Workspace;
-
+	class TextureId;
+	class UIEvent;
 	class MouseCommand : public RBX::INamed, public RBX::IRenderable {
 	private:
 		bool capturedMouse;
 	protected:
 		Workspace* workspace;
 		std::auto_ptr<XmlState> undoState;
-
-		virtual void capture();
 	public:
+		virtual RBX::MouseCommand* onMouseDown(const UIEvent&);
+	protected:
+		virtual void onMouseIdle(const UIEvent&);
+		virtual void onMouseHover(const UIEvent&);
+		virtual RBX::MouseCommand* onKeyDown(const UIEvent&);
+		virtual RBX::MouseCommand* onPeekKeyDown(const UIEvent&);
+		virtual void onMouseMove(const UIEvent&);
+		virtual void onMouseDelta(const UIEvent&);
+		virtual RBX::MouseCommand* onMouseUp(const UIEvent&);
+		virtual void capture();
+		virtual void releaseCapture();
+		virtual void cancel();
+	public:
+		virtual ~MouseCommand();
+		virtual TextureId getTextureId() const;
+		//Is this even in header???? If placed outside of header its adds a butt-ton of extra stack instructions so idk 
 		virtual MouseCommand* isSticky() const {
 			return 0;
 		}
