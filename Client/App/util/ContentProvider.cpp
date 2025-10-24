3c93601ba061c864a2ba1e84e1e91bfae127d2c7
ThePlayerRolo <thesolis78243@gmail.com>
2025-10-24 10:09:47 -0400

    ContentId work

---
+++ Client/App/util/ContentProvider.cpp
@@ -0,0 +1,47 @@
+#include "util/ContentProvider.h"
+
+namespace RBX
+{
+	ContentId::ContentId()
+		: id(), 
+		  mimeTypePtr(&Name::getNullName())
+	{
+	}
+
+	ContentId::ContentId(const char* id)
+		: id(id),
+		mimeTypePtr(&Name::getNullName())
+	{
+	}
+
+	ContentId::ContentId(std::string id)
+		: id(id),
+		mimeTypePtr(&Name::getNullName())
+	{
+	}
+
+	ContentId::ContentId(const ContentId& contentId)
+		: id(contentId.id),
+		  mimeTypePtr(contentId.mimeTypePtr)
+	{
+	}
+
+	ContentId::~ContentId()
+	{
+	}
+
+	bool ContentId::isAsset() const
+	{
+		return id.substr(0, 11) == "rbxasset://";
+	}
+
+	bool ContentId::isFile() const
+	{
+		return id.substr(0, 7) == "file://";
+	}
+
+	bool ContentId::isHttp() const
+	{
+		return id.substr(0, 4) == "http";
+	}
+}
