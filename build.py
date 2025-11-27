f725dc7a51af58f6e46d15e3b1344c6c355a851f
KeyboardCombination <gamerbear256@gmail.com>
2025-11-27 10:54:24 -0500

    auto configure when building

--- build.py
+++ build.py
@@ -1,10 +1,13 @@
 # TODO: find a cross platform way to do this
 import subprocess
 from pathlib import Path
+import configure
 
 root = Path(__file__).parent
 
 def build():
+    configure()
+    
     # shell lets us not have separate shells per call
     subprocess.run("C:\\Program Files (x86)\\Microsoft Visual Studio 8\\VC\\vcvarsall.bat", shell=True)
     subprocess.run(['msbuild', "RBXGSdecomp.sln", "/p:Configuration=Release"], shell=True, cwd=root)
